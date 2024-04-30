e065c9360546a1ced33f0490dd0b1080ec735282
Deiby26 <davidbohorquez150@gmail.com>
2024-04-30 14:34:05 -0500

    feat: Building registerPage

--- lib/registroUsuario.dart
+++ lib/registroUsuario.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class registerPage extends StatelessWidget {
+class registerPage extends StatefulWidget {
+  @override
+  State<registerPage> createState() => _registerPageState();
+}
+
+class _registerPageState extends State<registerPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +15,43 @@ class registerPage extends StatelessWidget {
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("HOLAMUNDO")],
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              child: Image(
+                  image: NetworkImage(
+                      'https://raw.githubusercontent.com/JackySeve/Alcancia-Movil/69537243881e5f5b9bf87200f7b14e4d0a660947/lib/assets/images/logo.png')),
+            ),
+            const Text(
+              "PocketMetrics",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "Registrate",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const Text("¡Bienvenido y espero llegues para quedarte!"),
+            Form(
+                child: Column(
+              children: [
+                SizedBox(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: const Text(
+                        "Usuario",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ))
+          ],
         )),
       ),
     );
